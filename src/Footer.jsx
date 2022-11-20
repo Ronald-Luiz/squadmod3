d7c1ac647dbcd92d941a4d45d72e0fffffdc2ff8
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2022-11-20 11:52:14 -0300

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import './Footer.css';
+
+function Rodape(){
+    return(
+
+<div className="rodape">
+
+<p>telefone: 2244-5566</p>
+
+<p>@gmail.com</p>     
+
+<p className="teste">serviços</p>     
+<br></br>
+<p>copyright @ Todos os direitos reservados</p>
+
+</div>
+
+
+);}
+
+export default Rodape;
